97ed55957e00d018b1605cd7d009a4a86f2d41a1
ThMRudolf <thomas.rudolf@itam.mx>
2024-01-09 19:36:19 -0600

    read xml fil names of column still contain name spaces

--- py/dtTMp1.py
+++ py/dtTMp1.py
@@ -1,19 +1,53 @@
 #Python code to illustrate parsing of XML files 
 # importing the required modules 
-import csv
-from errno import EDEADLK 
-import requests 
+# import csv
+# import requests
 import xml.etree.ElementTree as ET 
+import schedule
+import time
+import pandas as pd
 
+url_current = '../data/Trace_current.xml'
+tree = ET.parse(url_current)
+root = tree.getroot()
+##### test #####
+element = root.findall(".//")
+#print("Elements of .//")
+#print(element)
+print('Test start')
+for element in root.findall(".//"):
+    print("Next Element")
+    print(element.tag, element.text)
+##### test end#####
 
-def loadRSS(url_cnc_xml): 
-  
-    # url of rss feed 
-    url = 'http://www.hindustantimes.com/rss/topnews/rssfeed.xml'
-  
-    # creating HTTP response object from given url 
-    resp = requests.get(url) 
-  
-    # saving the xml file 
-    with open(url_cnc_url, 'wb') as f: 
-        f.write(resp.content) 
+# Create a list to store dictionaries representing each position info
+Samples_list = []
+# Iterate through each <WorkOffsetk> element
+print('start running')
+#print(root.findall(".//Samples"))
+## ./Streams/DeviceStream/ComponentStream/Samples
+for Samples_elem in root.findall('.//'):
+    Samples_dict = {}
+    
+    for child_elem in Samples_elem:
+        Samples_dict[child_elem.tag] = child_elem.text
+    Samples_list.append(Samples_dict)
+
+df_Samples = pd.DataFrame(Samples_list)
+print("resulting Samples:")
+#print(df_Samples)
+#def job():
+#    #response = requests.get(url)
+#    xml_data = url #response.text
+
+#    tree = ET.ElementTree(ET.fromstring(xml_data))
+#    root = tree.getroot()
+
+    # Now you can process or extract data from the XML using 'root' variable.
+
+# Schedule the job to run every minute
+#schedule.every(1).minutes.do(job)
+
+#while True:
+#    schedule.run_pending()
+#    time.sleep(1)
