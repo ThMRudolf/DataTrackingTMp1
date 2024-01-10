eb39293b6e634fc128e9bff448465614c3ef1ced
ThMRudolf <thomas.rudolf@itam.mx>
2024-01-09 20:59:38 -0600

    reading data form 'current' works. Starting upload to google cloud.

--- py/dtTMp1.py
+++ py/dtTMp1.py
@@ -8,6 +8,11 @@ import time
 import pandas as pd
 from lxml import etree
 
+## comm to google drive
+from io import BytesIO
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
 url_current = '../data/Trace_current.xml'
 
 def remove_namespace(tree):
@@ -41,6 +46,35 @@ def xml_to_dataframe(xml_file):
 df = xml_to_dataframe(url_current)
 
 # Now you have your XML data in a Pandas DataFrame
-print(df)
+#print(df)
 print(df.columns)
 print(df["PathPosition"].dropna())
+
+#print(df["AxisFeedrate"].dropna())
+
+
+print(df['Samples'].dropna())
+print(df["PathFeedrateOverride"].dropna())
+
+for idx in df.columns:
+    print(df[idx].dropna())
+
+
+
+## upload to google drive as CSV
+##
+# Convert DataFrame to CSV content in memory
+csv_content = BytesIO()
+df.to_csv(csv_content, index=False)
+csv_content.seek(0)  # Reset the file position to the beginning
+
+# Authenticate with Google Drive
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()  # Authorizes using your Google Drive credentials
+
+# Create GoogleDrive instance with authenticated GoogleAuth instance
+drive = GoogleDrive(gauth)
+
+# Upload the CSV file to Google Drive
+file_drive = drive.CreateFile({'title': 'your_file_name.csv'})  # Replace with your desired file name
+file_drive.Upload()
