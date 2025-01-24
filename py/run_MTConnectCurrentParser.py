19ec266e7edd9668078a509bc8841105549f5825
Alvaro Lopez <alvlopzam@icloud.com>
2025-01-24 17:50:08 -0600

    Trace_current.csv creation

--- py/run_MTConnectCurrentParser.py
+++ py/run_MTConnectCurrentParser.py
@@ -30,3 +30,17 @@ component_streams_df = pd.DataFrame(component_streams)
 #print("Component Streams DataFrame:")
 #print(component_streams_df)
 #print(component_streams_df['events'][1])
+
+#################################### Data Processing #############################
+events = []
+for (i,row) in component_streams_df.iterrows():
+    for event in row.events:
+        event["id"] = i
+    events.append(pd.DataFrame(component_streams_df.events[i]))
+component_streams_events_df = pd.concat(events, axis = 0)
+
+component_streams_final_df = component_streams_df.merge(component_streams_events_df,how="inner",left_index=True,right_on="id", suffixes=("","_event"))
+component_streams_final_df.reset_index(inplace=True)
+component_streams_final_df.drop(columns = ["events", "id", "id_event", "index"], inplace = True)
+
+component_streams_final_df.to_csv("../Output/Trace_current.csv", index=False)
